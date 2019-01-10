cdc485ee7cee5a25d0de242fe65d0426f88ff2fb
Tuyet Mai Tran <maittt@vietpointer.vn>
2019-01-10 17:18:57 +0700

    First Commit

---
+++ testcases/com/bankguru/login/Login_01_ValidateLoginForm.java
@@ -0,0 +1,39 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_01_ValidateLoginForm {
+	
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+  
+  @Test
+  public void TC01_LoginWithEmailInvalid() {
+  }
+  
+  @Test
+  public void TC02_LoginWithEmailIncorrect() {
+  }
+  
+  @Test
+  public void TC03_LoginWithPasswordLessThan6Chars() {
+  }
+
+  @Test
+  public void TC04_LoginWithPasswordIncorrect() {
+  }
+  
+  @Test
+  public void TC05_LoginWithUserAndPasswordValid() {
+  }
+
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
