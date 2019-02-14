dda9f413c25f5ebebcb6527975a51a991a71e6d9
Tuyet Mai Tran <maittt@vietpointer.vn>
2019-02-14 17:52:26 +0700

    Commit

--- actions/pageObjects/LoginPageObject.java
+++ actions/pageObjects/LoginPageObject.java
@@ -1,5 +1,9 @@
 package pageObjects;
 
 public class LoginPageObject {
+	
+	public void inputToUserIDTextbox() {
+		
+	}
 
 }
--- interfaces/pageUIs/LoginPageUI.java
+++ interfaces/pageUIs/LoginPageUI.java
@@ -1,5 +1,7 @@
 package pageUIs;
 
 public class LoginPageUI {
+	public static final String USERID_TEXTBOX ="//input[@name='uid']";
+	public String PASSWORD_TEXTBOX = "//input[@name='password']";
 
 }
